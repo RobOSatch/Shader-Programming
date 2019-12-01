fb0ea0ffd7d6c7fe00a7f4cf3ceb481933ceed82
RobOSatch <ro.barta@icloud.com>
2019-12-01 23:58:49 +0100

    Implements normal mapping

--- EZG-1/EZG-1.cpp
+++ EZG-1/EZG-1.cpp
@@ -30,6 +30,7 @@ unsigned int loadTexture(const char* path);
 void renderScene(const Shader& shader);
 void renderCube();
 void renderQuad();
+void renderWalls();
 
 // settings
 const unsigned int SCR_WIDTH = 1920;
@@ -52,14 +53,22 @@ float speed = 0.11f;
 
 bool visualize = false;
 
-// lighting
-//glm::vec3 lightPos(1.2f, 1.0f, 2.0f);
-
 Game_Mode gameMode = CREATE;
 
 std::vector<glm::vec3> waypoints;
 std::vector<glm::quat> orientations;
 
+// Textures
+unsigned int boxTexture;
+unsigned int wallTexture;
+unsigned int normalMap;
+unsigned int secondTexture;
+unsigned int secondNormalMap;
+
+Model *cube;
+glm::vec3 lightPos(0.0f, 5.0f, 6.0f);
+
+float bumpiness = 1.0f;
 
 int main()
 {
@@ -118,6 +127,7 @@ int main()
 		-25.0f, -0.5f, -25.0f,  0.0f, 1.0f, 0.0f,   0.0f, 25.0f,
 		 25.0f, -0.5f, -25.0f,  0.0f, 1.0f, 0.0f,  25.0f, 10.0f
 	};
+
 	// plane VAO
 	unsigned int planeVBO;
 	glGenVertexArrays(1, &planeVAO);
@@ -136,10 +146,16 @@ int main()
 	// load textures
 	// -------------
 	unsigned int woodTexture = loadTexture("resources/textures/wood.png");
+	boxTexture = loadTexture("resources/textures/container2.png");
+	wallTexture = loadTexture("resources/textures/brickwall.jpg");
+	normalMap = loadTexture("resources/textures/brickwall_normal.jpg");
+	
+	secondTexture = loadTexture("resources/textures/marmor.jpg");
+	secondNormalMap = loadTexture("resources/textures/marmor_normal.jpg");
 
 	// configure depth map FBO
 	// -----------------------
-	const unsigned int SHADOW_WIDTH = 1024, SHADOW_HEIGHT = 1024;
+	const unsigned int SHADOW_WIDTH = 8192, SHADOW_HEIGHT = 8192;
 	unsigned int depthMapFBO;
 	glGenFramebuffers(1, &depthMapFBO);
 	// create depth texture
@@ -160,17 +176,15 @@ int main()
 	glReadBuffer(GL_NONE);
 	glBindFramebuffer(GL_FRAMEBUFFER, 0);
 
-	// shader configuration
-	// --------------------
 	shader.use();
 	shader.setInt("diffuseTexture", 0);
-	shader.setInt("shadowMap", 1);
-
-	// lighting info
-	// -------------
-	glm::vec3 lightPos(-2.0f, 4.0f, -1.0f);
+	shader.setInt("normalMap", 1);
+	shader.setInt("shadowMap", 2);
 
 	std::vector<glm::vec3> curvePoints;
+
+	// Model stuff
+	cube = new Model("resources/objects/cube/cube.obj");
 
 	int currentWaypoint = 0;
 	float t = 0.0f;
@@ -178,14 +192,10 @@ int main()
 	// -----------
 	while (!glfwWindowShouldClose(window))
 	{
-		// per-frame time logic
-		// --------------------
 		float currentFrame = glfwGetTime();
 		deltaTime = currentFrame - lastFrame;
 		lastFrame = currentFrame;
 
-		// input
-		// -----
 		processInput(window);
 
 		// render
@@ -193,16 +203,13 @@ int main()
 		glClearColor(0.1f, 0.1f, 0.1f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-		// 1. render depth of scene to texture (from light's perspective)
-		// --------------------------------------------------------------
+		// render to depth map
 		glm::mat4 lightProjection, lightView;
 		glm::mat4 lightSpaceMatrix;
 		float near_plane = 1.0f, far_plane = 7.5f;
-		//lightProjection = glm::perspective(glm::radians(45.0f), (GLfloat)SHADOW_WIDTH / (GLfloat)SHADOW_HEIGHT, near_plane, far_plane); // note that if you use a perspective projection matrix you'll have to change the light position as the current light position isn't enough to reflect the whole scene
 		lightProjection = glm::ortho(-10.0f, 10.0f, -10.0f, 10.0f, near_plane, far_plane);
 		lightView = glm::lookAt(lightPos, glm::vec3(0.0f), glm::vec3(0.0, 1.0, 0.0));
 		lightSpaceMatrix = lightProjection * lightView;
-		// render scene from light's point of view
 		depthShader.use();
 		depthShader.setMat4("lightSpaceMatrix", lightSpaceMatrix);
 
@@ -210,7 +217,7 @@ int main()
 		glBindFramebuffer(GL_FRAMEBUFFER, depthMapFBO);
 		glClear(GL_DEPTH_BUFFER_BIT);
 		glActiveTexture(GL_TEXTURE0);
-		glBindTexture(GL_TEXTURE_2D, woodTexture);
+		glBindTexture(GL_TEXTURE_2D, wallTexture);
 		renderScene(depthShader);
 		glBindFramebuffer(GL_FRAMEBUFFER, 0);
 
@@ -231,24 +238,15 @@ int main()
 		shader.setVec3("viewPos", camera.Position);
 		shader.setVec3("lightPos", lightPos);
 		shader.setMat4("lightSpaceMatrix", lightSpaceMatrix);
+		shader.setFloat("bumpiness", bumpiness);
 		glActiveTexture(GL_TEXTURE0);
-		glBindTexture(GL_TEXTURE_2D, woodTexture);
+		glBindTexture(GL_TEXTURE_2D, wallTexture);
 		glActiveTexture(GL_TEXTURE1);
+		glBindTexture(GL_TEXTURE_2D, normalMap);
+		glActiveTexture(GL_TEXTURE2);
 		glBindTexture(GL_TEXTURE_2D, depthMap);
+		
 		renderScene(shader);
-
-
-		//if (gameMode == CREATE) {
-		//	// Draw waypoints
-		//	for (int i = 0; i < waypoints.size(); i++) {
-		//		model = glm::mat4(1.0f);
-		//		model = glm::translate(model, waypoints[i]);
-		//		model = glm::scale(model, glm::vec3(0.1f));
-		//		lampShader.setMat4("model", model);
-
-		//		glDrawArrays(GL_TRIANGLES, 0, 36);
-		//	}
-		//}
 
 		if (gameMode == RIDE) {
 			// Visualize curve
@@ -347,14 +345,50 @@ int main()
 // renders the 3D scene
 // --------------------
 void renderScene(const Shader& shader)
-{
-	// floor
+{	
 	glm::mat4 model = glm::mat4(1.0f);
+	//model = glm::rotate(model, glm::radians((float)glfwGetTime() * -30.f), glm::normalize(glm::vec3(1.0, 0.0, 1.0)));
+	shader.setMat4("model", model);
+	renderWalls();
+	
+	// Draw fancy stuff
+	for (int i = 1; i < 10; i++) {
+		for (int j = 1; j < 10; j++) {
+			model = glm::mat4(1.0f);
+			model = glm::translate(model, glm::vec3(2.0f * i, -1.5f, 2.0f * j));
+			shader.setMat4("model", model);
+			cube->Draw(shader);
+
+			if ((i == 1 && j == 1) || (i == 9 && j == 9) || (i == 1 && j == 9) || (i == 9 && j == 1)) {
+				for (int k = 0; k < 5; k++) {
+					model = glm::mat4(1.0f);
+					model = glm::translate(model, glm::vec3(2.0f * i, -1.5f + 2.0f * k, 2.0f * j));
+					shader.setMat4("model", model);
+					cube->Draw(shader);
+				}
+			}
+		}
+	}
+
+	model = glm::mat4(1.0f);
+	model = glm::translate(model, glm::vec3(6.0, 2.0f, 6.0));
+	model = glm::rotate(model, glm::radians(60.0f), glm::normalize(glm::vec3(1.0, 0.0, 1.0)));
+	model = glm::scale(model, glm::vec3(0.3f));
+	shader.setMat4("model", model);
+	glActiveTexture(GL_TEXTURE0);
+	glBindTexture(GL_TEXTURE_2D, secondTexture);
+	glActiveTexture(GL_TEXTURE1);
+	glBindTexture(GL_TEXTURE_2D, secondNormalMap);
+	glActiveTexture(GL_TEXTURE2);
+	cube->Draw(shader);
+
+	// floor
+	/*model = glm::mat4(1.0f);
 	shader.setMat4("model", model);
 	glBindVertexArray(planeVAO);
-	glDrawArrays(GL_TRIANGLES, 0, 6);
+	glDrawArrays(GL_TRIANGLES, 0, 6);*/
 	// cubes
-	model = glm::mat4(1.0f);
+	/*model = glm::mat4(1.0f);
 	model = glm::translate(model, glm::vec3(0.0f, 1.5f, 0.0));
 	model = glm::scale(model, glm::vec3(0.5f));
 	shader.setMat4("model", model);
@@ -369,7 +403,7 @@ void renderScene(const Shader& shader)
 	model = glm::rotate(model, glm::radians(60.0f), glm::normalize(glm::vec3(1.0, 0.0, 1.0)));
 	model = glm::scale(model, glm::vec3(0.25));
 	shader.setMat4("model", model);
-	renderCube();
+	renderCube();*/
 }
 
 
@@ -479,6 +513,102 @@ void renderQuad()
 	glBindVertexArray(0);
 }
 
+// renders a 1x1 quad in NDC with manually calculated tangent vectors
+// ------------------------------------------------------------------
+unsigned int wallVAO = 0;
+unsigned int wallVBO;
+void renderWalls()
+{
+	if (quadVAO == 0)
+	{
+		// positions
+		glm::vec3 pos1(-1.0f, 1.0f, 0.0f);
+		glm::vec3 pos2(-1.0f, -1.0f, 0.0f);
+		glm::vec3 pos3(1.0f, -1.0f, 0.0f);
+		glm::vec3 pos4(1.0f, 1.0f, 0.0f);
+		// texture coordinates
+		glm::vec2 uv1(0.0f, 1.0f);
+		glm::vec2 uv2(0.0f, 0.0f);
+		glm::vec2 uv3(1.0f, 0.0f);
+		glm::vec2 uv4(1.0f, 1.0f);
+		// normal vector
+		glm::vec3 nm(0.0f, 0.0f, 1.0f);
+
+		// calculate tangent/bitangent vectors of both triangles
+		glm::vec3 tangent1, bitangent1;
+		glm::vec3 tangent2, bitangent2;
+		// triangle 1
+		// ----------
+		glm::vec3 edge1 = pos2 - pos1;
+		glm::vec3 edge2 = pos3 - pos1;
+		glm::vec2 deltaUV1 = uv2 - uv1;
+		glm::vec2 deltaUV2 = uv3 - uv1;
+
+		GLfloat f = 1.0f / (deltaUV1.x * deltaUV2.y - deltaUV2.x * deltaUV1.y);
+
+		tangent1.x = f * (deltaUV2.y * edge1.x - deltaUV1.y * edge2.x);
+		tangent1.y = f * (deltaUV2.y * edge1.y - deltaUV1.y * edge2.y);
+		tangent1.z = f * (deltaUV2.y * edge1.z - deltaUV1.y * edge2.z);
+		tangent1 = glm::normalize(tangent1);
+
+		bitangent1.x = f * (-deltaUV2.x * edge1.x + deltaUV1.x * edge2.x);
+		bitangent1.y = f * (-deltaUV2.x * edge1.y + deltaUV1.x * edge2.y);
+		bitangent1.z = f * (-deltaUV2.x * edge1.z + deltaUV1.x * edge2.z);
+		bitangent1 = glm::normalize(bitangent1);
+
+		// triangle 2
+		// ----------
+		edge1 = pos3 - pos1;
+		edge2 = pos4 - pos1;
+		deltaUV1 = uv3 - uv1;
+		deltaUV2 = uv4 - uv1;
+
+		f = 1.0f / (deltaUV1.x * deltaUV2.y - deltaUV2.x * deltaUV1.y);
+
+		tangent2.x = f * (deltaUV2.y * edge1.x - deltaUV1.y * edge2.x);
+		tangent2.y = f * (deltaUV2.y * edge1.y - deltaUV1.y * edge2.y);
+		tangent2.z = f * (deltaUV2.y * edge1.z - deltaUV1.y * edge2.z);
+		tangent2 = glm::normalize(tangent2);
+
+
+		bitangent2.x = f * (-deltaUV2.x * edge1.x + deltaUV1.x * edge2.x);
+		bitangent2.y = f * (-deltaUV2.x * edge1.y + deltaUV1.x * edge2.y);
+		bitangent2.z = f * (-deltaUV2.x * edge1.z + deltaUV1.x * edge2.z);
+		bitangent2 = glm::normalize(bitangent2);
+
+
+		float quadVertices[] = {
+			// positions            // normal         // texcoords  // tangent                          // bitangent
+			pos1.x, pos1.y, pos1.z, nm.x, nm.y, nm.z, uv1.x, uv1.y, tangent1.x, tangent1.y, tangent1.z, bitangent1.x, bitangent1.y, bitangent1.z,
+			pos2.x, pos2.y, pos2.z, nm.x, nm.y, nm.z, uv2.x, uv2.y, tangent1.x, tangent1.y, tangent1.z, bitangent1.x, bitangent1.y, bitangent1.z,
+			pos3.x, pos3.y, pos3.z, nm.x, nm.y, nm.z, uv3.x, uv3.y, tangent1.x, tangent1.y, tangent1.z, bitangent1.x, bitangent1.y, bitangent1.z,
+
+			pos1.x, pos1.y, pos1.z, nm.x, nm.y, nm.z, uv1.x, uv1.y, tangent2.x, tangent2.y, tangent2.z, bitangent2.x, bitangent2.y, bitangent2.z,
+			pos3.x, pos3.y, pos3.z, nm.x, nm.y, nm.z, uv3.x, uv3.y, tangent2.x, tangent2.y, tangent2.z, bitangent2.x, bitangent2.y, bitangent2.z,
+			pos4.x, pos4.y, pos4.z, nm.x, nm.y, nm.z, uv4.x, uv4.y, tangent2.x, tangent2.y, tangent2.z, bitangent2.x, bitangent2.y, bitangent2.z
+		};
+		// configure plane VAO
+		glGenVertexArrays(1, &wallVAO);
+		glGenBuffers(1, &wallVBO);
+		glBindVertexArray(wallVAO);
+		glBindBuffer(GL_ARRAY_BUFFER, wallVBO);
+		glBufferData(GL_ARRAY_BUFFER, sizeof(quadVertices), &quadVertices, GL_STATIC_DRAW);
+		glEnableVertexAttribArray(0);
+		glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 14 * sizeof(float), (void*)0);
+		glEnableVertexAttribArray(1);
+		glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 14 * sizeof(float), (void*)(3 * sizeof(float)));
+		glEnableVertexAttribArray(2);
+		glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 14 * sizeof(float), (void*)(6 * sizeof(float)));
+		glEnableVertexAttribArray(3);
+		glVertexAttribPointer(3, 3, GL_FLOAT, GL_FALSE, 14 * sizeof(float), (void*)(8 * sizeof(float)));
+		glEnableVertexAttribArray(4);
+		glVertexAttribPointer(4, 3, GL_FLOAT, GL_FALSE, 14 * sizeof(float), (void*)(11 * sizeof(float)));
+	}
+	glBindVertexArray(wallVAO);
+	glDrawArrays(GL_TRIANGLES, 0, 6);
+	glBindVertexArray(0);
+}
+
 // process all input: query GLFW whether relevant keys are pressed/released this frame and react accordingly
 // ---------------------------------------------------------------------------------------------------------
 void processInput(GLFWwindow * window)
@@ -505,6 +635,11 @@ void processInput(GLFWwindow * window)
 		speed = fmin(0.5f, speed + 0.0001f);
 	if (glfwGetKey(window, GLFW_KEY_DOWN) == GLFW_PRESS)
 		speed = fmax(0.0f, speed - 0.0001f);
+
+	if (glfwGetKey(window, GLFW_KEY_RIGHT) == GLFW_PRESS)
+		bumpiness = fmin(1.0f, bumpiness + 0.02f);
+	if (glfwGetKey(window, GLFW_KEY_LEFT) == GLFW_PRESS)
+		bumpiness = fmax(0.0f, bumpiness - 0.02f);
 }
 
 void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods)
--- EZG-1/lightShader.frag
+++ EZG-1/lightShader.frag
@@ -6,30 +6,30 @@ in VS_OUT {
     vec3 Normal;
     vec2 TexCoords;
     vec4 FragPosLightSpace;
+	vec3 TangentLightPos;
+	vec3 TangentViewPos;
+	vec3 TangentFragPos;
 } fs_in;
 
 uniform sampler2D diffuseTexture;
+uniform sampler2D normalMap;
 uniform sampler2D shadowMap;
 
 uniform vec3 lightPos;
 uniform vec3 viewPos;
 
+uniform float bumpiness;
+
 float ShadowCalculation(vec4 fragPosLightSpace)
 {
-    // perform perspective divide
     vec3 projCoords = fragPosLightSpace.xyz / fragPosLightSpace.w;
-    // transform to [0,1] range
     projCoords = projCoords * 0.5 + 0.5;
-    // get closest depth value from light's perspective (using [0,1] range fragPosLight as coords)
     float closestDepth = texture(shadowMap, projCoords.xy).r; 
-    // get depth of current fragment from light's perspective
     float currentDepth = projCoords.z;
-    // calculate bias (based on depth map resolution and slope)
     vec3 normal = normalize(fs_in.Normal);
     vec3 lightDir = normalize(lightPos - fs_in.FragPos);
     float bias = max(0.05 * (1.0 - dot(normal, lightDir)), 0.005);
-    // check whether current frag pos is in shadow
-    // float shadow = currentDepth - bias > closestDepth  ? 1.0 : 0.0;
+
     // PCF
     float shadow = 0.0;
     vec2 texelSize = 1.0 / textureSize(shadowMap, 0);
@@ -43,7 +43,6 @@ float ShadowCalculation(vec4 fragPosLightSpace)
     }
     shadow /= 9.0;
     
-    // keep the shadow at 0.0 when outside the far_plane region of the light's frustum.
     if(projCoords.z > 1.0)
         shadow = 0.0;
         
@@ -51,24 +50,35 @@ float ShadowCalculation(vec4 fragPosLightSpace)
 }
 
 void main()
-{           
+{       
+	vec3 lightColor = vec3(0.6);
+	
+	vec3 normal = texture(normalMap, fs_in.TexCoords).rgb;
+	normal = normalize(normal * 2.0 - 1.0);
+	normal.xy *= bumpiness;
+    normal = normalize(normal);
+	//vec3 shadowNormal = normalize(fs_in.Normal);
+
     vec3 color = texture(diffuseTexture, fs_in.TexCoords).rgb;
-    vec3 normal = normalize(fs_in.Normal);
-    vec3 lightColor = vec3(0.3);
-    // ambient
+        
+	// ambient
     vec3 ambient = 0.3 * color;
-    // diffuse
-    vec3 lightDir = normalize(lightPos - fs_in.FragPos);
+    
+	// diffuse
+    //vec3 lightDir = normalize(lightPos - fs_in.FragPos);
+	vec3 lightDir = normalize(fs_in.TangentLightPos - fs_in.TangentFragPos);
     float diff = max(dot(lightDir, normal), 0.0);
     vec3 diffuse = diff * lightColor;
-    // specular
-    vec3 viewDir = normalize(viewPos - fs_in.FragPos);
+    
+	// specular
+    //vec3 viewDir = normalize(viewPos - fs_in.FragPos);
+	vec3 viewDir = normalize(fs_in.TangentViewPos - fs_in.TangentFragPos);
     vec3 reflectDir = reflect(-lightDir, normal);
-    float spec = 0.0;
     vec3 halfwayDir = normalize(lightDir + viewDir);  
-    spec = pow(max(dot(normal, halfwayDir), 0.0), 64.0);
-    vec3 specular = spec * lightColor;    
-    // calculate shadow
+    float spec = pow(max(dot(normal, halfwayDir), 0.0), 64.0);
+    vec3 specular = spec * lightColor;  
+    
+	// shadow
     float shadow = ShadowCalculation(fs_in.FragPosLightSpace);                      
     vec3 lighting = (ambient + (1.0 - shadow) * (diffuse + specular)) * color;    
     
